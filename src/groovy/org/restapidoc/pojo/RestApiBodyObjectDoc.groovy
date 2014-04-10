013ad6879a6c25331be0edac3931fb97f43aa135
Loïc Rollus <loicrollus@gmail.com>
2014-04-10 15:51:09 +0200

    add missing files

---
+++ src/groovy/org/restapidoc/pojo/RestApiBodyObjectDoc.groovy
@@ -0,0 +1,31 @@
+package org.restapidoc.pojo
+
+import org.jsondoc.core.pojo.ApiBodyObjectDoc
+import org.jsondoc.core.util.JSONDocUtils
+
+import java.lang.reflect.Method;
+
+public class RestApiBodyObjectDoc extends ApiBodyObjectDoc{
+    public String jsondocId = UUID.randomUUID().toString();
+//    private String object;
+//    private String multiple;
+//    private String mapKeyObject;
+//    private String mapValueObject;
+//    private String map;
+
+//    public static RestApiBodyObjectDoc buildFromAnnotation(Method method) {
+//        boolean multiple = false;
+//        Integer index = -1; // to implement (ssee jsondoc source)
+//        if (index != -1) {
+//            Class<?> parameter = method.getParameterTypes()[index];
+//            multiple = JSONDocUtils.isMultiple(parameter);
+//            return new RestApiBodyObjectDoc(getBodyObject(method, index)[0], getBodyObject(method, index)[1], getBodyObject(method, index)[2], String.valueOf(multiple), getBodyObject(method, index)[3]);
+//        }
+//        return null;
+//    }
+
+    public RestApiBodyObjectDoc(String object, String mapKeyObject, String mapValueObject, String multiple, String map) {
+        super(object,mapKeyObject,mapValueObject,multiple,map);
+    }
+
+}
---
+++ src/groovy/org/restapidoc/pojo/RestApiErrorDoc.groovy
@@ -0,0 +1,40 @@
+package org.restapidoc.pojo
+
+import org.restapidoc.annotation.RestApiError
+import org.restapidoc.annotation.RestApiErrors
+
+/**
+ * Created by lrollus on 4/10/14.
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class RestApiErrorDoc {
+    public String jsondocId = UUID.randomUUID().toString();
+    private String code;
+    private String description;
+
+    public static List<RestApiErrorDoc> buildFromAnnotation(RestApiErrors annotation) {
+        List<RestApiErrorDoc> apiMethodDocs = new ArrayList<RestApiErrorDoc>();
+        for (RestApiError apiError : annotation.apierrors()) {
+            apiMethodDocs.add(new RestApiErrorDoc(apiError.code(), apiError.description()));
+        }
+        return apiMethodDocs;
+    }
+
+    public RestApiErrorDoc(String code, String description) {
+        this.code = code
+        this.description = description
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
